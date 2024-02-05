fdef95992fa8360374899c9a0212039a28aeffae
Prasanna V B <prasannavb04@gmail.com>
2024-02-05 23:40:32 +0530

    Final Full Project

--- src/Loading/Loading.jsx
+++ src/Loading/Loading.jsx
@@ -1,9 +1,17 @@
+//CSS
+import './Loading.css'
+
 const Loading=()=>
 {
     return(
-        <>
-            <h2>Loading</h2>
-        </>
+       <div className="Loading-Page">
+         <div className='Loading'>
+        <div className="Loading-div-1"></div>
+        <div className="Loading-div-2"></div>
+        <div className="Loading-div-3"></div>
+        <div className="Loading-div-4"></div>
+        </div>
+       </div>
     )
 }
 
